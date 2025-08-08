0403c3b52ad48406269a949f63c6a76f57c274e0
Error Boy <boyerror928@gmail.com>
2025-08-08 17:28:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// var val1 = prompt(" enter your first value ")
+// var sign = prompt(" enter your sign ")
+// var val2 = prompt(" enter your second value ")
+
+// console.log(val1 + sign + val2)
+
+// if (sign === " + ") {
+//     alert((+ val1) + (+ val2))
+// }
+// else if (sign === " - ") {
+//     alert(val1 - val2)
+// }
+// else if (sign === " * ") {
+//     alert(val1 * val2)
+// }
+// else if (sign === " / ") {
+//     alert(val1 / val2)
+// }
+
+
+
+
+
+
